f93f8c71d7eb6a46b08fe41f64be218c2f6bbde1
App Academy Student <student@appacademy.io>
2013-09-26 14:57:53 -0700

    Move "move" to board.rb

--- board.rb
+++ board.rb
@@ -9,11 +9,6 @@ module Checkers
       @board = init_board
     end
 
-    def move(start_pos, end_pos)
-      piece = piece(start_pos)
-      piece.perform_moves(start_pos, end_pos)
-    end
-
     def show_board
       background = [:white, :cyan]
       bg_toggle = 0
@@ -28,24 +23,6 @@ module Checkers
         bg_toggle = (bg_toggle == 0 ? 1 : 0)
       end
     end
-
-    def piece(pos)
-      @board[pos[0]][pos[1]]
-    end
-
-    def []=(pos, value)
-      @board[pos[0]][pos[1]] = value
-    end
-
-    def occupied?(pos)
-      !piece(pos).nil?
-    end
-
-    def occupied_by?(pos, color)
-      piece(pos) && piece(pos).color == color
-    end
-
-    private
 
     def init_board
       board = Array.new(8) { Array.new(8) { nil } }
@@ -67,6 +44,95 @@ module Checkers
       board
     end
 
+    def move(start_pos, end_pos)
+      if valid_slide_move?(start_pos, end_pos)
+        perform_slide(start_pos, end_pos)
+      elsif valid_jump_move?(start_pos, end_pos)
+        perform_jump(start_pos, end_pos)
+      else
+        raise InvalidMoveException.new("Invalid move!")
+      end
+    end
+
+    def perform_moves!(move_sequence)
+    end
+
+    def perform_slide(start_pos, end_pos)
+      piece = piece(start_pos)
+      self[start_pos] = nil
+      if (end_pos[0] == 0) && (piece.color == :white)
+#        self[end_pos] = KingPiece.new(end_pos, piece.color, self)
+      elsif (end_pos[0] == 7) && (piece.color == :black)
+#        self[end_pos] = KingPiece.new(end_pos, piece.color, self)
+      else
+        self[end_pos] = piece
+      end
+    end
+
+    def perform_jump(start_pos, end_pos)
+      mid_pos = [(start_pos[0] + end_pos [0]) / 2, (start_pos[1] + end_pos [1]) / 2]
+      self[mid_pos] = nil
+
+      perform_slide(start_pos, end_pos)
+    end
+
+    private
+
+    def valid_slide_move?(start_pos, end_pos)
+      return false if occupied?(end_pos) || out_of_range?(start_pos, end_pos)
+
+      piece = piece(start_pos)
+      valid_moves = []
+      piece.slide_moves[piece.color].each do |valid_move|
+        valid_moves << [start_pos[0] + valid_move[0], start_pos[1] + valid_move[1]]
+      end
+      return false unless valid_moves.include?(end_pos)
+      true
+    end
+
+    def valid_jump_move?(start_pos, end_pos)
+      return false if occupied?(end_pos) || out_of_range?(start_pos, end_pos)
+
+      valid_moves = []
+      piece = piece(start_pos)
+      piece.jump_moves[piece.color].each do |valid_move|
+        valid_moves << [start_pos[0] + valid_move[0], start_pos[1] + valid_move[1]]
+      end
+
+      return false unless valid_moves.include?(end_pos)
+
+      mid_pos = [(start_pos[0] + end_pos [0]) / 2, (start_pos[1] + end_pos [1]) / 2]
+      false if occupied_by?(mid_pos, other_color(piece.color))
+      true
+    end
+
+    def other_color(color)
+      color == :white ? :black : :white
+    end
+
+    def out_of_range?(start_pos, end_pos)
+      move_sequence = [start_pos, end_pos]
+      return false if move_sequence.all? { |move| move[0].between?(0,7) && move[1].between?(0,7) }
+      true
+    end
+
+    def piece(pos)
+      @board[pos[0]][pos[1]]
+    end
+
+    def []=(pos, value)
+      i,j = pos
+      @board[i][j] = value
+    end
+
+    def occupied?(pos)
+      !piece(pos).nil?
+    end
+
+    def occupied_by?(pos, color)
+      piece(pos) && piece(pos).color == color
+    end
+
   end
 end
 
@@ -81,4 +147,15 @@ checkers.show_board
 checkers.move([2,4], [4,6])
 checkers.show_board
 checkers.move([1,3], [3,1])
+checkers.show_board
+checkers.move([1,5], [2,4])
+checkers.show_board
+
+checkers.move([5,5], [3,7])
+checkers.show_board
+checkers.move([3,7], [1,5])
+checkers.show_board
+checkers.move([0,4], [1,3])
+checkers.show_board
+checkers.move([1,5], [0,4])
 checkers.show_board
--- king_piece.rb
+++ king_piece.rb
@@ -1,4 +1,5 @@
 require_relative './piece.rb'
+
 module Checkers
   class KingPiece < Piece
     attr_accessor :color
